eb9836cfe42be9a77ad7508812d0d9b0f6402970
eliab_hdez <77374111+EliabHdez@users.noreply.github.com>
2023-04-12 13:13:59 -0600

    probando el alert en github page

--- script.js
+++ script.js
@@ -5,11 +5,5 @@ let codigoPostal
 const eventoMultiple = document.getElementById('evento')
 
 eventoMultiple.addEventListener('click', (e) => {
-    let codigoPostal = prompt('Ingresa el codigo postal de destino')
-    console.log(codigoPostal)
-    if (codigoPostal == 54715){
-        alert(`SI tenemos cobertura en el codigo postal ${codigoPostal}`)
-    }else{
-        alert(`NO tenemos cobertura en el codigo postal ingresado (${codigoPostal})`)
-    }
+    let codigoPostal = alert('El alert si funciona')
 })
